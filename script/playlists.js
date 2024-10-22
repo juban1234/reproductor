d2bcf85d5597e9dcf83b3de7423dd9bb9644ab08
Sena <dimatesebas73@gmail.com>
2024-10-22 17:22:16 -0500

    actualizacion

--- script/playlists.js
+++ script/playlists.js
@@ -6,15 +6,27 @@ export const playlists = [
       song: 'canciones/Death Grips - Lost Boys - Death Grips (youtube).mp3'
     },
     {
-      img: 'iconos/img1.jpg',
-      artist: 'Death hola',
-      title: 'Lost Boys',
-      song: 'canciones/Death Grips - Lost Boys - Death Grips (youtube).mp3'
+      img: 'iconos/sddefault.jpg',
+      artist: 'Katy Perry',
+      title: 'Dark house',
+      song: 'canciones/Katy Perry - Dark Horse (Lyrics) ft. Juicy J - LOL (youtube).mp3'
     },
     {
-      img: 'iconos/img1.jpg',
-      artist: 'Death Grips',
-      title: 'Lost Boys',
-      song: 'canciones/Death Grips - Lost Boys - Death Grips (youtube).mp3'
+      img: 'iconos/bloodline.png',
+      artist: 'RIKDIX',
+      title: 'bloodline',
+      song: 'canciones/Bloodline - Evolving Sound - RIKDIX (youtube).mp3'
+    },
+    {
+      img: 'iconos/vanity.png',
+      artist: 'Brand X Music',
+      title: 'Vanity ',
+      song: 'canciones/Vanity - Brand X Music (youtube).mp3'
+    },
+    {
+      img: 'iconos/my ordinary life.png',
+      artist: 'The Living Tombstone',
+      title: 'My Ordinary Life',
+      song: 'canciones/My Ordinary Life-The Living Tombstone - The Living Tombstone (youtube).mp3'
     }
   ]
